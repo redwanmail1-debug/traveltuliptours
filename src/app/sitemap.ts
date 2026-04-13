7d2e408c8649a83b224e23bb4f86ba07193ba0bf
Redoes <Redoesss@MacBookPro.home>
2026-04-13 19:01:50 +0200

    Sitemap vereenvoudigen, anchor links verwijderen voor betere SEO indexering

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -8,29 +8,5 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: "weekly",
       priority: 1,
     },
-    {
-      url: "https://traveltuliptours.nl/#tours",
-      lastModified: new Date(),
-      changeFrequency: "weekly",
-      priority: 0.9,
-    },
-    {
-      url: "https://traveltuliptours.nl/#booking",
-      lastModified: new Date(),
-      changeFrequency: "monthly",
-      priority: 0.8,
-    },
-    {
-      url: "https://traveltuliptours.nl/#faq",
-      lastModified: new Date(),
-      changeFrequency: "monthly",
-      priority: 0.7,
-    },
-    {
-      url: "https://traveltuliptours.nl/#contact",
-      lastModified: new Date(),
-      changeFrequency: "monthly",
-      priority: 0.6,
-    },
   ];
 }
